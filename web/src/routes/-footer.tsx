05888acfe3e63d9ea5dc22e7ee43d22dfa72715c
igorb <igorbabin2002@gmail.com>
2026-04-20 14:55:28 +0500

    Added issue link

--- web/src/routes/-footer.tsx
+++ web/src/routes/-footer.tsx
@@ -1,14 +1,22 @@
 import {Button, Surface} from "@heroui/react";
-import {ExternalLink} from "lucide-react";
+import {Bug, ExternalLink} from "lucide-react";
 
 
 export default function Footer() {
 
     return <footer>
         <Surface variant='tertiary' className='border-t p-2 flex flex-row justify-between items-center'>
-            <a href='https://github.com/MindHardt/Cact' target='_blank'>
-                <img height='32' width='32' src='/github.svg' alt='GitHub' />
-            </a>
+            <div className='flex flex-row gap-2 items-center'>
+                <a href='https://github.com/MindHardt/Cact' target='_blank'>
+                    <img height='32' width='32' src='/github.svg' alt='GitHub' />
+                </a>
+                <a href='https://github.com/MindHardt/Cact/issues/new' target='_blank'>
+                    <Button variant='secondary'>
+                        <Bug />
+                        <span className='hidden sm:inline'>Нашли ошибку?</span>
+                    </Button>
+                </a>
+            </div>
             <a href='https://un1ver5e.tech/' target='_blank'>
                 <Button variant='secondary'>
                     Разработчик
